0ce0b70e03db4aaf5c62e03634c5ac885bf35519
Serge <117431814+SergeAbroyann@users.noreply.github.com>
2023-03-14 23:26:06 +0400

    Fix

--- lib/enum/task_status_enum.dart
+++
@@ -1,1 +0,0 @@
-enum TaskStatusEnum { all, active, completed }
--- lib/screens/typedef_functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:to_do_app/enum/task_status_enum.dart';
-
-typedef StringCallback = Function(String value);
-typedef DoubleDateTimeCallback = Function(DateTime? value1, DateTime? value2);
-typedef DynamicCallback = Function(dynamic value);
-typedef IndexCallback = Function(int index);
-typedef BoolCallback = Function(bool value);
-typedef DateTimeCallback = Function(DateTime? date);
-typedef TaskStatusEnumCallback = Function(TaskStatusEnum taskStatusEnum);
